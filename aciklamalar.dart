b3782371fc0dec44c92690babfd36de504574e6d
Resul Canca <75572823+cncresul@users.noreply.github.com>
2025-03-25 12:39:14 +0300

    Add files via upload

---
+++ aciklamalar.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  print("Resul CANCA");
+  print("Resul");
+  print("Resul\'un dersleri");
+  print(3*5);
+  // + Metinleri birleştirme
+  print("Resul" + " CANCA");
+
+/*
+  print("deneme")
+  print("deneme")
+  print("deneme")
+*/
+
+
+}
---
+++ number_veri_tip.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  int yas = 24;
+  print(yas);
+
+  double sicaklik = 25.5;
+  print(sicaklik);
+
+  num kilo = 105;
+  print(kilo);
+
+  var yil = 1955;
+  print(yil);
+}
